a071f6f99ed6a2d01209e886aec3cbd5a8ae7843
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-08-22 08:43:57 -0400

    gradient clipping

--- simplelinearnn_train_improve.py
+++ simplelinearnn_train_improve.py
@@ -80,6 +80,7 @@ def run(params):
             # Backward and optimize
             optimizer.zero_grad() # Clear gradients
             loss.backward()       # Compute gradients
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()      # Update weights
 
         # Validation
